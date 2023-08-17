7885f42809e971633f975476b696d194beeefafd
Teemu Spoof <tspoof@student.hive.fi>
2023-08-17 19:26:47 +0300

    Unnecessary violence

---
+++ ex03/HumanB.cpp
@@ -0,0 +1,13 @@
+#include "HumanB.hpp"
+
+HumanB::HumanB(std::string name): name(name){};
+
+void	HumanB::attack(void) const
+{
+	std::cout << name << " attacks with their " << weapon->getType() << std::endl;
+}
+
+void	HumanB::setWeapon(Weapon &weapon)
+{
+	this->weapon = &weapon;
+}
