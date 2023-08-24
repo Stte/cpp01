991743912706e15cce0f4b8c67b912b8ce5a2748
Teemu Spoof <tspoof@student.hive.fi>
2023-08-24 12:55:39 +0300

    ex04 done

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -4,36 +4,41 @@
 int	main(int argc, char *argv[])
 {
 	if (argc != 4)
+	{
+		std::cout << "Usage: ./replace file find replace" << std::endl;
 		return (1);
+	}
+
 	std::string	filename = argv[1];
 	std::string	s1 = argv[2];
 	std::string	s2 = argv[3];
 
+	if (filename.empty() || s1.empty() || s2.empty() || !s1.compare(s2))
+	{
+		std::cout << "Usage: ./replace file find replace" << std::endl;
+		return (2);
+	}
 	std::ifstream fileIn(filename);
 	if (!fileIn.is_open())
-		return (2);
-
-
+	{
+		std::cout << "Error: failed to open input file" << std::endl;
+		return (3);
+	}
 	std::ofstream fileOut(filename + ".replace");
 	if(!fileOut.is_open())
-		return (3);
-
+	{
+		std::cout << "Error: failed to create output file" << std::endl;
+		return (4);
+	}
 	std::string	line;
-	size_t		idx;
+	size_t		index;
 	while (std::getline(fileIn, line))
 	{
-		idx = line.find(s1);
-		while (idx != std::string::npos && line.compare(""))
+		while ((index = line.find(s1)) != std::string::npos)
 		{
-
+			line.erase(index, s1.length());
+			line.insert(index, s2);
 		}
-		if (idx == std::string::npos)
-		{
-			fileOut << line << std::endl;
-			continue;
-		}
-		line.erase(idx, s1.length());
-		line.insert(idx, s2);
 		fileOut << line << std::endl;
 	}
 	fileIn.close();
